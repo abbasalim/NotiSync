ce5441c352dc76ba7fc74796abcaa1d03cd5fcb5
Abbasali <abbasali88m@yahoo.com>
2025-09-24 19:57:14 +0330

    siz

--- composeApp/src/jvmMain/kotlin/com/esfandune/ui/MainScreen.kt
+++ composeApp/src/jvmMain/kotlin/com/esfandune/ui/MainScreen.kt
@@ -1,32 +1,57 @@
 package com.esfandune.ui
 
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ClearAll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.esfandune.util.packageToEmoji
 import com.esfandune.NotificationManager
 import com.esfandune.NotificationServer
 import com.esfandune.model.NotificationData
 import com.esfandune.ui.theme.AppTheme
+import com.esfandune.util.packageToEmoji
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import java.awt.Desktop
 import java.net.Inet4Address
 import java.net.NetworkInterface
+import java.net.URI
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -63,7 +88,7 @@ fun MainApp() {
                 label = { Text("همه") },
                 modifier = Modifier.padding(end = 4.dp)
             )
-            notificationManager.notifications.distinctBy { it.first().packageName } .forEach { app ->
+            notificationManager.notifications.distinctBy { it.first().packageName }.forEach { app ->
                 FilterChip(
                     selected = selectedPackage == app.first().packageName,
                     onClick = { selectedPackage = app.first().packageName },
@@ -91,6 +116,16 @@ fun MainApp() {
                             text = "NotiSync",
                             style = MaterialTheme.typography.headlineSmall
                         )
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            Desktop.getDesktop().browse(URI("http://tools.esfandune.ir/"))
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Info,
+                                contentDescription = "درباره"
+                            )
+                        }
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
                         containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -137,11 +172,11 @@ fun MainApp() {
                 }
 
 
-
                 // App filter chips
-                  FilterApps()
+                FilterApps()
 
-                val notifs = notificationManager.notifications.filter { selectedPackage==null || it.first().packageName == selectedPackage }
+                val notifs =
+                    notificationManager.notifications.filter { selectedPackage == null || it.first().packageName == selectedPackage }
                 if (notifs.isEmpty()) {
                     Box(
                         modifier = Modifier
@@ -162,22 +197,22 @@ fun MainApp() {
                         modifier = Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        item{
+                        item {
 
                         }
                         items(
                             items = notifs,
                             itemContent = { notification ->
-                                    NotificationCard(
-                                        notifications = notification,
-                                        modifier = Modifier.animateItem(),
-                                        onMarkAsRead = {
-                                            coroutineScope.launch {
-                                                sendReadConfirmation(notification)
-                                                notificationManager.markAsRead(notification)
-                                            }
+                                NotificationCard(
+                                    notifications = notification,
+                                    modifier = Modifier.animateItem(),
+                                    onMarkAsRead = {
+                                        coroutineScope.launch {
+                                            sendReadConfirmation(notification)
+                                            notificationManager.markAsRead(notification)
                                         }
-                                    )
+                                    }
+                                )
                             }
                         )
                     }
@@ -188,7 +223,6 @@ fun MainApp() {
 
 
 }
-
 
 
 private fun getDeviceIp(): String = NetworkInterface.getNetworkInterfaces()
