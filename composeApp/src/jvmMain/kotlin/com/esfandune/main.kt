f543efc32ff0878692c47fefb8e11960f83e0ca7
Abbasali <abbasali88m@yahoo.com>
2025-09-24 19:50:15 +0330

    siz

--- composeApp/src/jvmMain/kotlin/com/esfandune/main.kt
+++ composeApp/src/jvmMain/kotlin/com/esfandune/main.kt
@@ -1,6 +1,10 @@
 package com.esfandune
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowSize
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.esfandune.ui.MainApp
 import notisync.composeapp.generated.resources.Res
@@ -13,6 +17,10 @@ fun main() = application {
         icon = icon,
         onCloseRequest = ::exitApplication,
         title = "NotiSync",
+        state = WindowState(
+//            position = WindowPosition(100.dp, 100.dp),
+            size = DpSize(500.dp, 800.dp)
+        )
     ) {
         MainApp()
     }
