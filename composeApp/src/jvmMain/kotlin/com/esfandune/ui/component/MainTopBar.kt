e4372538074bb7c1ba82fac5594cf3d24d50cccd
Abbasali <abbasali88m@yahoo.com>
2025-09-27 14:18:44 +0330

    hlp

--- composeApp/src/jvmMain/kotlin/com/esfandune/ui/component/MainTopBar.kt
+++ composeApp/src/jvmMain/kotlin/com/esfandune/ui/component/MainTopBar.kt
@@ -1,6 +1,7 @@
 package com.esfandune.ui.component
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Help
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.NotificationsOff
@@ -8,8 +9,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import com.esfandune.NotificationManager
+import com.esfandune.ui.HelpDialog
 import kotlinx.coroutines.launch
 import java.awt.Desktop
 import java.net.URI
@@ -21,6 +27,10 @@ fun MainTopBar(
     showSnackbar: (String) -> Unit,
 ) {
     val coroutineScope = rememberCoroutineScope()
+    var showHelpDialog by remember { mutableStateOf(false) }
+    if (showHelpDialog){
+        HelpDialog(onDismiss = { showHelpDialog = false })
+    }
     IconButton(onClick = {
         notificationManager.toggleSilentMode()
         coroutineScope.launch {
@@ -36,6 +46,15 @@ fun MainTopBar(
         )
     }
     IconButton(onClick = {
+        showHelpDialog = true
+    }) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.Help,
+            contentDescription = "راهنما"
+        )
+    }
+
+    IconButton(onClick = {
         Desktop.getDesktop().browse(URI("http://tools.esfandune.ir/"))
     }) {
         Icon(
@@ -43,4 +62,5 @@ fun MainTopBar(
             contentDescription = "درباره"
         )
     }
+
 }
