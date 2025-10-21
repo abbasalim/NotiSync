1466f220bc12f2e783342a4f445e8d34addf90bb
Abbasali <abbasali88m@yahoo.com>
2025-10-21 18:02:18 +0330

    Revert "Update app version"

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -82,8 +82,8 @@ android {
         applicationId = "com.esfandune.notisync"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.1"
     }
     
     lint {
