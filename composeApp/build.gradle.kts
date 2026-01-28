b057c99d201966874853be71d65a2c736be24d6c
Abbasali <abbasali88m@yahoo.com>
2026-01-28 09:19:18 +0330

    versionCode = 3         versionName = "1.2"

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -82,8 +82,8 @@ android {
         applicationId = "com.esfandune.notisync"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
     
     lint {
